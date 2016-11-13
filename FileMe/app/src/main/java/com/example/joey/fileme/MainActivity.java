3f49e3a61ab802855005b835d0b543461554e579
Evan <woodring@iastate.edu>
2016-11-13 17:17:55 -0600

    Finished Homescreen

--- FileMe/app/src/main/java/com/example/joey/fileme/MainActivity.java
+++ FileMe/app/src/main/java/com/example/joey/fileme/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.joey.fileme;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.example.joey.fileme.request_image.RequestImageActivity;
 import com.example.joey.fileme.upload_image.UploadImageActivity;
@@ -15,6 +19,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView i = (ImageView) findViewById(R.id.background);
+        i.setImageBitmap(((BitmapDrawable)getDrawable(R.drawable.filecabinet)).getBitmap());
     }
 
     public void launchRequestImageActivity(View v) {
